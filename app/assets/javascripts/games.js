fdcbe7c00a546d8d01c01f52cc08a35cee579090
Jack Nolan <jack@jacknolan.com>
2012-11-17 23:57:00 -0500

    migrated to int for prices

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -17,37 +17,36 @@ $(function(){
     bProcessing: true,
     bServerSide: true,
     sScrollY: "600px",
-    sAja2xSource: $('#games').data('source'),
+    sAjaxSource: $('#games').data('source'),
     "sDom": "fp<'span6 preparing'r>"
   });
-   // $('#games tbody').delegate("tr", "click", rowClick);
+   $('#games tbody').delegate("tr", "click", rowClick);
 })
-// function createInfo(info) {
-//   info = JSON.parse(info)
-//   console.log(info)
-//  $('.info h3.title span').replaceWith("<span>" + info.name + "</span>");
-//  $('.info li.released span').replaceWith("<span>" + info.release_date + "</span>");
-//   // $('.info').replaceWith('<h1 class='title'>' + info.name + '</h1>');
-//   // $('.info').replaceWith('<h1 class='title'>' + info.name + '</h1>');
-//   // $('.info').replaceWith('<h1 class='title'>' + info.name + '</h1>');
+function createInfo(info) {
+  info = JSON.parse(info)
+  console.log(info)
+ $('.info h3.title span').replaceWith("<span>" + info.name + "</span>");
+ $('.info li.released span').replaceWith("<span>" + info.release_date + "</span>");
+ $('.info li.steam_link a').replaceWith('<a href="' + info.url + '">Steam Link</a>');
+  // $('.info').replaceWith('<h1 class='title'>' + info.name + '</h1>');
 
-// }
-// function createPriceHistory () {
-
-
-// }
-// function createExtras () {
+}
+function createPriceHistory () {
 
 
-// }
-// function rowClick(){
-//  //expand and populated oth parts with cool shit
+}
+function createExtras () {
 
-//  $(".selected").removeClass("selected")
-//  $(this).addClass('selected');
-//  $.getJSON($(this).find('td a').attr('href') + ".json",function(data){
-//   createInfo(data[0]);
-//   // $('.priceHistory').append('<p>' + data[1] + '</p>');
-//   // $('.extras').append('<p>' + data[2] + '</p>');
-//  });
-// }
+
+}
+function rowClick(){
+ //expand and populated oth parts with cool shit
+
+ $(".selected").removeClass("selected")
+ $(this).addClass('selected');
+ $.getJSON($(this).find('td a').attr('href') + ".json",function(data){
+  createInfo(data[0]);
+  // $('.priceHistory').append('<p>' + data[1] + '</p>');
+  // $('.extras').append('<p>' + data[2] + '</p>');
+ });
+}
